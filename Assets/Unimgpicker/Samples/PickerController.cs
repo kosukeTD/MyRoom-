673cd7b168396eee202426552ef0d9727bbdfe4d
kosuke <kosuke.n@tandemdesign.co.jp>
2017-09-22 00:54:07 +0900

    オリジナルアプリ
    メンタリングで教わった部分まで反映しました

--- Assets/Unimgpicker/Samples/PickerController.cs
+++ Assets/Unimgpicker/Samples/PickerController.cs
@@ -11,14 +11,17 @@ namespace Kakera
         [SerializeField]
         private MeshRenderer imageRenderer;
 
-		//追記
+		//追記 1 
 		public static string imagePickingObjectName;
+		//追記 2 Materialsの配列を指定するためのインデックス
+		int index = 0;
 
 
         void Awake()
         {
             imagePicker.Completed += (string path) =>
-            {				
+            {			
+			    //下記を使うとpickerControllerに設定したマテリアルが全部変わってしまう	
                 //StartCoroutine(LoadImage(path, imageRenderer));
 				//下記変更内容
 				if(imagePickingObjectName == gameObject.name){
@@ -29,23 +32,28 @@ namespace Kakera
 
         public void OnPressShowPicker()
         {
-			//下記追記
+			//下記追記 1
 			imagePickingObjectName = gameObject.name;
 			Debug.Log(gameObject.name);
 
-            imagePicker.Show("Select Image", "unimgpicker", 1024);
+			//for分追記 2 これでimagePicker.Showは「画像を選んで貼る」まで行ってくれるので、マテリアルの数だけ実行すれば全てのマテリアルに画像が貼られる。
+			for (int i = 0; i < imageRenderer.materials.Length; i++) {
+				imagePicker.Show("Select Image", "unimgpicker", 1024);
+
+			}
+
         }
 
         private IEnumerator LoadImage(string path, MeshRenderer output)
         {
-			//下記追記
-			Debug.Log("aa:"+gameObject.name);
+			//下記追記 1
+			//Debug.Log("aa:"+gameObject.name);
 
             var url = "file://" + path;
             var www = new WWW(url);
             yield return www;
 
-			//下記追記
+			//下記追記 1
 			//Debug.Log("aaaaaaaaaaaaaaaaaa");
 
 
@@ -55,7 +63,11 @@ namespace Kakera
                 Debug.LogError("Failed to load texture url:" + url);
             }
 
-            output.material.mainTexture = texture;
+			//下記変更 2
+            //output.material.mainTexture = texture;
+			output.materials[index].mainTexture = texture;
+			index++;
+
         }
     }
 }
