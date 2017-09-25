6e3c2b35cbc381e9398a5fcfc671ac22ae010af7
kosuke <kosuke.n@tandemdesign.co.jp>
2017-09-25 17:31:51 +0900

    オリジナルアプリ
    テクスチャの一斉貼り替えがうまくいきません

--- Assets/Script/PickerControllerAll.cs
+++ Assets/Script/PickerControllerAll.cs
@@ -35,6 +35,9 @@ namespace Kakera
 					StartCoroutine(LoadImage(path, imageRenderer2));
 				}
 			};
+
+
+
 		}
 
 		public void OnPressShowPicker()
@@ -73,7 +76,7 @@ namespace Kakera
 
 			//下記変更 2
 			//output.material.mainTexture = texture;
-			output.sharedMaterials[index].mainTexture = texture;
+			output.materials[index].mainTexture = texture;
 			//下記2回目の設定値
 			index++;
 			//テクスチャの数が複数ある場合はエラーが出るので下記を追加
@@ -92,3 +95,68 @@ namespace Kakera
 		}
 	}
 }
+
+		/*
+		public void OnPressShowPicker()
+		{
+			//下記追記 1
+			imagePickingObjectName = gameObject.name;
+			Debug.Log(gameObject.name);
+
+			//for分追記 2 これでimagePicker.Showは「画像を選んで貼る」まで行ってくれるので、マテリアルの数だけ実行すれば全てのマテリアルに画像が貼られる。
+			for (int i = 0; i < imageRenderer.materials.Length; i++) {
+				imagePicker.Show("Select Image", "Unimgpicker", 1024);
+			}
+
+		}
+
+		private IEnumerator LoadImage(string path, MeshRenderer output)
+		{
+			//下記追記 3 デフォルトのテクスチャを貼る
+			for (int i = 0; i < imageRenderer.materials.Length; i++) {
+				imageRenderer.sharedMaterials [i].mainTexture = null; 
+			}
+
+			//下記追記 1
+			//Debug.Log("aa:"+gameObject.name);
+
+			var url = "file://" + path;
+			var www = new WWW(url);
+			yield return www;
+
+			//下記追記 1
+			//Debug.Log("aaaaaaaaaaaaaaaaaa");
+
+			//下記は違う
+			//this.imageRenderer = GetComponent()Materials[0];
+			//this.imageRenderer2 = GetComponent()Materials[1]
+
+
+
+			var texture = www.texture;
+			if (texture == null)
+			{
+				Debug.LogError("Failed to load texture url:" + url);
+			}
+
+			//下記変更 2
+			//output.material.mainTexture = texture;
+			output.sharedMaterials[index].mainTexture = texture;
+			//下記2回目の設定値
+			index++;
+			//テクスチャの数が複数ある場合はエラーが出るので下記を追加
+			if (imageRenderer.materials.Length <= index) {
+				index = 0;
+			}
+
+			//for(int i = 0; i < imageRenderer.materials.Length; i++){
+			//	index++;
+			//};
+			//index = 0;
+
+
+
+
+		}
+	}
+}*/
---
+++ Assets/Unimgpicker/Scripts/PickeriOSAll.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_IOS
+using System.Runtime.InteropServices;
+
+namespace Kakera
+{
+    internal class PickeriOSAll : IPicker
+    {
+        [DllImport("__Internal")]
+        private static extern void UnimgpickerAll_show(string title, string outputFileName, int maxSize);
+
+        public void Show(string title, string outputFileName, int maxSize)
+        {
+            UnimgpickerAll_show(title, outputFileName, maxSize);
+        }
+    }
+}
+#endif
