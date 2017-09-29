9895bbf2428056fa7363bc1d3fe73f71d834a6cc
kosuke <kosuke.n@tandemdesign.co.jp>
2017-09-30 00:28:36 +0900

    オリジナルアプリ
    とりあえず現状

--- Assets/Script/ChaseTest.cs
+++ Assets/Script/ChaseTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chase : MonoBehaviour {
+public class ChaseTest: MonoBehaviour {
 
 
 	public Transform target1;//追いかける対象-オブジェクトをインスペクタから登録できるように
@@ -16,15 +16,12 @@ public class Chase : MonoBehaviour {
 
 		void Start () {
 			//target = GameObject.Find("対象").transform; インスペクタから登録するのでいらない
-	//	CheckPoint1 = GameObject.Find("CheckPoint1");
-	//	CheckPoint2 = GameObject.Find("CheckPoint2");
-	//	CheckPoint3 = GameObject.Find("CheckPoint3");
 
 		}
 
 		void Update () {
 			//targetの方に少しずつ向きが変わる
-			//transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (target1.position - transform.position), 0.3f);
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (target1.position - transform.position), 0.3f);
 
 			//targetに向かって進む
 			//transform.position += transform.forward * speed;
