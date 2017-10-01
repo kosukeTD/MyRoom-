b74b44fdaf78a0f99340b4bf429d9b0430e28ed1
kosuke <kosuke.n@tandemdesign.co.jp>
2017-10-01 20:56:31 +0900

    オリジナルアプリ
    先生に指示されたスクリプトを修正して挙動が正確に！

---
+++ Assets/Script/PickerControllerAll3.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kakera
+{
+    public class PickerControllerAll3 : MonoBehaviour
+	{
+		[SerializeField]
+		private Unimgpicker imagePicker;
+
+		[SerializeField]
+		private MeshRenderer imageRenderer;
+
+		[SerializeField]
+		private MeshRenderer imageRenderer2;
+
+		[SerializeField]
+		private MeshRenderer imageRenderer3;
+
+		[SerializeField]
+		private MeshRenderer imageRenderer4;
+
+		[SerializeField]
+		private MeshRenderer[] imageRenderers;
+
+		//追記　3 共通のテクスチャにするために下記を削除
+		//追記 1 
+		//public static string imagePickingObjectName;
+
+		//追記 2 Materialsの配列を指定するためのインデックス
+		int index = 0;
+
+		void Awake()
+		{
+			imagePicker.Completed += (string path) =>
+			{			
+				//下記を使うとpickerControllerに設定したマテリアルが全部変わってしまう	
+				//StartCoroutine(LoadImage(path, imageRenderer));
+				//下記変更内容
+				if(PickerController.imagePickingObjectName == gameObject.name){
+					for(int i=0;i< imageRenderers.Length;i++){						
+						StartCoroutine(LoadImage(path, imageRenderers[i]));
+					}
+
+				}
+				//if(PickerController.imagePickingObjectName == gameObject.name){
+				//	StartCoroutine(LoadImage(path, imageRenderer2));
+				//}
+			};
+
+
+
+		}
+
+		public void OnPressShowPicker()
+		{
+			//下記追記 1
+			PickerController.imagePickingObjectName = gameObject.name;
+			StartCoroutine (ShowCoroutine ());
+
+
+		}
+
+		//下記、テクスチャを正確に貼り替えるために修正したスクリプトを追記
+		IEnumerator ShowCoroutine(){
+			//レンダラーのマテリアルの数だけループする
+			for (int i = 0; i < imageRenderer.materials.Length; i++) {
+				//選択画面表示
+				imagePicker.Show("Select Image", "unimgpicker", 1024);
+				//１秒待つ：テクスチャが張り替えられるのを待つ
+				yield return new WaitForSeconds (1);
+				//張り替え終わったらindexを更新する
+				index++;
+			}
+			//全て張り替え終わったらindexをリセットする
+			index = 0;
+		}
+
+
+		private IEnumerator LoadImage(string path, MeshRenderer output)
+		{
+
+			//下記追記 1
+			//Debug.Log("aa:"+gameObject.name);
+
+			var url = "file://" + path;
+			var www = new WWW(url);
+			yield return www;
+
+			//下記追記 1
+			//Debug.Log("aaaaaaaaaaaaaaaaaa");
+
+
+			var texture = www.texture;
+			if (texture == null)
+			{
+				Debug.LogError("Failed to load texture url:" + url);
+			}
+
+			//下記変更 2
+			//output.material.mainTexture = texture;
+			output.materials[index].mainTexture = texture;
+			//下記2回目の設定値
+			//index++; //IEnumerator ShowCoroutine()を追加時に削除
+			//テクスチャの数が複数ある場合はエラーが出るので下記を追加
+			if (imageRenderer.materials.Length <= index) {
+				index = 0;
+			}
+
+			//for(int i = 0; i < imageRenderer.materials.Length; i++){
+			//	index++;
+			//};
+			//index = 0;
+
+
+
+
+		}
+	}
+}
